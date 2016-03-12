8b651a6cf0eefeb28b96bfc4508d2bae4ee3b56b
aribs <alejoribes@gmail.com>
2016-03-12 02:10:18 +0100

    create QuestionObject

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 /**
  * Created by alejoribes on 11/3/16.
  */
-
+var GlobalQuestion = '';
 $(document).ready(function(){
+
     ArrayQuestion = [];
-    getTextQuestion(22)
-    getTextQuestion(13);
-    getTextQuestion(42);
-    var question1 = new Question(22);
-    
-    console.log(question1);
+    getTextQuestion(22);
+    var Question1 = new Question(22);
+    console.log(Question1);
+
+
+
 
 });
 
 function getTextQuestion(number){
     var url = 'http://numbersapi.com/' + number + '/trivia?fragment'
-    $.ajax({
+   var textQuestion =  $.ajax({
         url: url,
         type: 'GET',
         data: '',
-        dataType: "jsonp",
         success:function(response){saveQuestion(response)},
-        error:function(){console.log("error, number not found")}
+        error:function(){console.log("error, number not found")},
+
     });
 };
 function saveQuestion(response){
-    return response;
-
+    GlobalQuestion = response;
 
 };
-function printArray(){
-    console.log(ArrayQuestion);
-};
+
 
 function Question(number){
     this.answer = number
     this.badResponse1 = Math.round(Math.random() * (100 - 0) + 0);
     this.badResponse2 = Math.round(Math.random() * (100 - 0) + 0);
     this.badResponse3 = Math.round(Math.random() * (100 - 0) + 0);
+    this.questionText = GlobalQuestion;
 };
